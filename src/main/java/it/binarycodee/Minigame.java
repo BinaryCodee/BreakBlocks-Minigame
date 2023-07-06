aa5026aed90d626cd4abb372173898bb51bf6616
BuildTools <unconfigured@null.spigotmc.org>
2023-07-05 12:27:54 -1200

    Plugin base #1-ChangeLogs BreakBlocks-Minigame v0.0.1

---
+++ src/main/java/it/binarycodee/Minigame.java
@@ -0,0 +1,17 @@
+package it.binarycodee;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BreakBlocks_Minigame extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
